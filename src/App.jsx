e2cbc8d5d9ce67f3edb2d67087c3814207792513
phaothu2002x <truongthuan21342@gmail.com>
2025-07-08 01:45:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+
+const App = () => {
+    return (
+        <main>
+            <Welcome />
+        </main>
+    );
+};
+
+export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Welcome = () => {
+    return <h1 className="text-3xl font-bold text-center my-10">Welcome</h1>;
+};
+
+export default Welcome;
