afae30a1d17a2e520fd8bd6cfa5dd13a5a711246
phaothu2002x <truongthuan21342@gmail.com>
2025-07-11 02:54:03 +0700

    flip animation with scaling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import gsap from 'gsap';
-import { ScrollTrigger, SplitText } from 'gsap/all';
+import { ScrollTrigger, SplitText, Flip } from 'gsap/all';
 import Welcome from './components/Welcome';
 import HeroSection from './components/HeroSection';
 import TextAnim from './components/TextAnim';
 import MaskImaged from './components/MaskImaged';
 import MaskedText from './components/MaskedText';
-
-gsap.registerPlugin(ScrollTrigger, SplitText);
+import FlipTechnique from './components/FlipTechnique';
+gsap.registerPlugin(ScrollTrigger, SplitText, Flip);
 const App = () => {
     return (
         <main>
@@ -15,6 +15,7 @@ const App = () => {
             <TextAnim />
             <MaskImaged />
             <MaskedText />
+            <FlipTechnique />
             <div className=" my-10 min-h-dvh bg-amber-200" />
         </main>
     );
---
+++ src/components/FlipTechnique.jsx
@@ -0,0 +1,58 @@
+import gsap from 'gsap';
+import { Flip } from 'gsap/all';
+import React, { useRef, useState } from 'react';
+
+const FlipTechnique = () => {
+    const flipBoxRef = useRef(null);
+    const [moved, setMoved] = useState(false);
+    const handleClick = () => {
+        const box = flipBoxRef.current;
+        const state = Flip.getState(box);
+
+        if (moved) {
+            document.querySelector('.parentContainer').appendChild(box);
+            // Apply scale transform before animation
+            gsap.set(box, { scale: 1 });
+        } else {
+            document.querySelector('.newContainer').appendChild(box);
+            // Reset scale when moving back
+            gsap.set(box, { scale: 2 });
+        }
+
+        Flip.from(state, {
+            duration: 1.5,
+            ease: 'power1.inOut',
+            scale: true,
+            absolute: true,
+            onComplete: () => {
+                gsap.to(box, {
+                    scale: moved ? 1 : 2,
+                    duration: 0.5,
+                });
+            },
+        });
+        console.log(moved);
+
+        setMoved(!moved);
+    };
+    return (
+        <div
+            className="parentContainer w-full h-[50vh] bg-teal-400 relative"
+            id="flipBox"
+        >
+            <div ref={flipBoxRef} className={`flipBox w-[50px] h-[50px]`}>
+                <p>Hi</p>
+            </div>
+            <div className="newContainer "></div>
+
+            <button
+                onClick={handleClick}
+                className="absolute bottom-0 left-1/2 -translate-x-1/2 bg-blue-500 w-fit p-5 rounded-2xl cursor-pointer"
+            >
+                <p className="font-bold text-2xl">Click to change</p>
+            </button>
+        </div>
+    );
+};
+
+export default FlipTechnique;
