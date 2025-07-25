d788cf261bf0100858cc6017f25ed76690ba51cb
phaothu2002x <truongthuan21342@gmail.com>
2025-07-26 02:11:24 +0700

    3d text stagger seamlessly

--- src/components/Text3d.jsx
+++ src/components/Text3d.jsx
@@ -7,25 +7,36 @@ const Text3d = () => {
 
     useGSAP(
         () => {
-            const animation = gsap.timeline({ repeat: -1 });
-            const stagger = 1;
+            const animation = gsap.timeline({});
+            const numberOfTarget = 5;
+            const duration = 0.3;
+            const pause = 1;
+            const stagger = duration + pause;
+            const repeatDelay = stagger * (numberOfTarget - 1) + pause;
             animation
+                .set('.demo div', { transformOrigin: '50% 50% -50' })
                 .from('.demo div', {
-                    y: 100,
+                    rotateX: -90,
+                    // rotateY: -40,
                     opacity: 0,
-                    ease: 'power2.in',
+                    duration: duration,
                     stagger: {
                         each: stagger,
+                        repeatDelay: repeatDelay,
+                        repeat: -1,
                     },
                 })
                 .to(
                     '.demo div',
                     {
-                        y: -100,
+                        rotateX: 90,
+                        // rotateY: 40,
                         opacity: 0,
-                        ease: 'power2.in',
+                        duration: duration,
                         stagger: {
                             each: stagger,
+                            repeat: -1,
+                            repeatDelay: repeatDelay,
                         },
                     },
                     stagger
@@ -36,12 +47,12 @@ const Text3d = () => {
     return (
         <main ref={textRef} id="text3d" className="boxContent bg-amber-300">
             <div className="wrapper-content">
-                <div className="demo">
-                    <div>Wonderful guys</div>
-                    <div>this is 3d rotation</div>
-                    <div>Never give up</div>
-                    <div>keep going</div>
-                    <div>It's gonna be Ok😁</div>
+                <div className="demo  text-blue-900 font-extraboldbold">
+                    <div>WONDERFULL Ryan truong</div>
+                    <div>THIS IS 3D ROTATION TEXT</div>
+                    <div>Never Give Up</div>
+                    <div>Keep Going HiHi!</div>
+                    <div>It's Gonna Be Ok😁</div>
                 </div>
             </div>
         </main>
